cc6170276585052519c072ded354b2cdada7d407
Dominik Mrázek <mrazekdominik@centrum.cz>
2023-07-15 11:35:21 +0200

    Add error message to wrong input

--- TowerLocatorApp/ClientApp/src/app/components/file-upload/file-upload.component.ts
+++ TowerLocatorApp/ClientApp/src/app/components/file-upload/file-upload.component.ts
@@ -10,17 +10,16 @@ export class FileUploadComponent {
   isInvalidFileFormat: boolean = true;
 
   checkFileFormat(file: File, format: string): void {
-    if (file) {                       /*jestlize je vybran nejaky soubor, tak otestuju jeho priponu*/
-      const fileName = file.name;     /*vytahnu si jmeno */
-      const fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase(); /*odseparuji priponu*/
-      if (fileExtension !== format) {  /*jestlize je pripona spravna, tak vratim, FALSE*/
+    if (file) {                                       /*jestlize je vybran nejaky soubor, tak otestuju jeho priponu*/
+      const fileName = file.name;                     /*vytahnu si jmeno */
+      const fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();                               /*odseparuji priponu*/
+      if (fileExtension !== format) {                 /*jestlize je pripona spravna, tak vratim, FALSE*/
         this.isInvalidFileFormat = true;
       } else {
         this.isInvalidFileFormat = false;
       }
     } else {
-      this.isInvalidFileFormat =
-        true;                         /*kdyz neni vybran soubor, tak je to FALSE */
+      this.isInvalidFileFormat =true; /*kdyz neni vybran soubor, tak je to FALSE */
     }
   }
 }
