fbea94f755534c00653f6506774523f624e0d12b
Dominik Mrázek <mrazekdominik@centrum.cz>
2023-07-15 10:11:51 +0200

    Create basic file upload component, button disabled in Name is empty

--- TowerLocatorApp/ClientApp/src/app/components/file-upload/file-upload.component.ts
+++ TowerLocatorApp/ClientApp/src/app/components/file-upload/file-upload.component.ts
@@ -1,13 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.css'],
 })
 export class FileUploadComponent {
-  
-  }
-
+  routeName: string = '';
+}
